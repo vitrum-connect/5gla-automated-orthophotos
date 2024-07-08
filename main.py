53fc39fe193ad71d6bb44fc65d7425704a4098c3
Lukas Lange <ll@blue-sky-ops.de>
2024-07-08 09:36:41 +0200

    Refactored Script and changed IMAGE_DIR and API_KEY to ENV Variables

--- main.py
+++ main.py
@@ -1,5 +1,50 @@
+import os
+
+from fastapi import FastAPI, Depends, HTTPException
+from fastapi.security.api_key import APIKeyHeader, APIKey
+from starlette.status import HTTP_403_FORBIDDEN
+
 from nodeodm_client import NodeodmClient
 
-for i in range(5):
-    nodeodm_client = NodeodmClient()
-    nodeodm_client.calculate_orthophoto()
+app = FastAPI()
+
+
+IMAGE_DIR = os.getenv('IMAGE_DIR')
+API_KEY = os.getenv('API_KEY')
+print(f"API_KEY")
+print(API_KEY)
+print(f"IMAGE_DIR")
+print(IMAGE_DIR)
+API_KEY_NAME = "access_token"
+
+api_key_header = APIKeyHeader(name=API_KEY_NAME, auto_error=False)
+
+
+async def get_api_key(
+        api_key_header: str = Depends(api_key_header)
+):
+    if api_key_header == API_KEY:
+        return api_key_header
+    else:
+        raise HTTPException(
+            status_code=HTTP_403_FORBIDDEN, detail="Could not validate credentials"
+        )
+
+
+@app.get("/calculate_orthophoto/{transaction_id}", responses={
+    200: {"description": "Task created successfully"},
+    404: {"description": "No images for transaction ID in S3 bucket"},
+})
+async def calculate_orthophoto(transaction_id: str, api_key: APIKey = Depends(get_api_key)):
+    """ Calculates an orthophoto for a given transaction ID
+
+    :param api_key: The API key to authenticate the request
+    :param transaction_id: The transaction ID to calculate the orthophoto for
+    :return: The task ID of the created task or an HttpException 404 if an error occurred
+
+    """
+    nodeodm_client = NodeodmClient(IMAGE_DIR)
+    task_id = await nodeodm_client.calculate_orthophoto(transaction_id)
+    if task_id is None:
+        raise HTTPException(status_code=404, detail="No images for transaction ID in S3 bucket")
+    return {"uuid": task_id}
--- nodeodm_client/nodeodm_client.py
+++ nodeodm_client/nodeodm_client.py
@@ -1,6 +1,6 @@
+import asyncio
 import json
 import os
-import time
 
 import requests
 
@@ -23,11 +23,12 @@ class NodeodmClient:
 
     """
 
-    def __init__(self):
+    def __init__(self, image_dir):
         temp = os.path.dirname(os.path.abspath(__file__))
         self.project_dir = os.path.dirname(temp)
-        self.local_image_dir = os.path.join(self.project_dir, 'images')
-        self.odm_output_dir = os.path.join(self.project_dir, 'odm_output')
+        # self.local_image_dir = os.path.join(self.project_dir, 'images')
+        self.image_dir = image_dir
+        # self.odm_output_dir = os.path.join(self.project_dir, 'odm_output')
         self.nodeodm_url = 'http://192.168.2.39:4000'
         self.logger = CustomLogger()
 
@@ -61,25 +62,20 @@ class NodeodmClient:
         return True
 
     def get_task_status(self, task_id):
-        response = self.http_get(f'{self.nodeodm_url}/task/{task_id}/info')
-        response_json = json.loads(response.text)
-        if response.status_code != 200:
-            self.logger.log_warning(f"Error getting task status for task {task_id}: {response.text}")
+        try:
+            response = self.http_get(f'{self.nodeodm_url}/task/{task_id}/info')
+            response_json = json.loads(response.text)
+            if response.status_code != 200:
+                self.logger.log_warning(f"Error getting task status for task {task_id}: {response.text}")
+                return None
+            status_code = response_json['status']['code']
+            return status_code
+        except Exception as e:
+            self.logger.log_warning(f"An error occurred: {str(e)}")
             return None
-        status_code = response_json['status']['code']
-        return status_code
 
-    def create_task(self, images_path, options):
-        """ Creates a new task in NodeODM
-
-        :param images_path: The path to the images directory
-        :param options: The NodeODM task options
-        :return: The task ID of the created task or None if an error occurred
-
-        """
-        files = [(f'images', open(os.path.join(images_path, f), 'rb')) for f in os.listdir(images_path) if
-                 os.path.isfile(os.path.join(images_path, f))]
-        response = self.http_post(f'{self.nodeodm_url}/task/new', files=files, data=options)
+    def create_task_new_init(self, options):
+        response = self.http_post(f'{self.nodeodm_url}/task/new/init', data=options)
         response_json = json.loads(response.text)
         if response.status_code != 200:
             self.logger.log_warning(f"Error creating task: {response.text}")
@@ -87,6 +83,24 @@ class NodeodmClient:
         task_id = response_json['uuid']
         self.logger.log_info(f'Creating task with UUID: {task_id} .')
         return task_id
+
+    def task_new_commit(self, task_id):
+        response = self.http_post(f'{self.nodeodm_url}/task/new/commit/{task_id}', data={})
+        if response.status_code != 200:
+            self.logger.log_warning(f"Error creating task: {response.text}")
+            return None
+        self.logger.log_info(f'Images were committed to task with UUID: {task_id} .')
+        return True
+
+    def task_new_upload(self, task_id, images_path):
+        files = [(f'images', open(os.path.join(images_path, f), 'rb')) for f in os.listdir(images_path) if
+                 os.path.isfile(os.path.join(images_path, f))]
+        response = self.http_post(f'{self.nodeodm_url}/task/new/upload/{task_id}', files=files, data={})
+        if response.status_code != 200:
+            self.logger.log_warning(f"Error creating task: {response.text}")
+            return None
+        self.logger.log_info(f'Images were uploaded to task with UUID: {task_id} .')
+        return True
 
     def download_results(self, task_id, output_dir):
         response = self.http_get(f'{self.nodeodm_url}/task/{task_id}/download/all.zip')
@@ -99,10 +113,36 @@ class NodeodmClient:
             f.write(content)
         self.logger.log_info(f"Results downloaded to {zip_path}")
 
-    def calculate_orthophoto(self):
-        # Workflow
+    async def process_task(self, task_id, transaction_image_dir):
         try:
-            # Define task options
+            self.task_new_upload(task_id, transaction_image_dir)
+            self.task_new_commit(task_id)
+            status_code = 0
+            while status_code != 40 and status_code != 30 and status_code != 50:
+                status_code = self.get_task_status(task_id)
+                self.logger.log_info(switch_case(status_code))
+                if status_code is None:
+                    self.logger.log_warning("Error getting task status.")
+                    return
+                await asyncio.sleep(60)
+
+            if status_code == 30 or status_code == 50:
+                return
+            if status_code == 40:
+                self.download_results(task_id, transaction_image_dir)
+                self.remove_task(task_id)
+        except Exception as e:
+            self.logger.log_warning(f"An error occurred: {str(e)}")
+
+    async def calculate_orthophoto(self, transaction_id):
+        """
+        Calculates an orthophoto for a given transaction ID. This function creates a new task on NodeODM and uploads the images.
+        The function is not waiting for the task to finish, but returns the task ID.
+
+        :param transaction_id: The transaction ID to calculate the orthophoto for
+        :return: The task ID of the created task or None if an error occurred
+        """
+        try:
             task_options = {
                 'end-with': 'odm_orthophoto',
                 'feature-quality': 'high',
@@ -110,28 +150,16 @@ class NodeodmClient:
                 'fast-orthophoto': 'true',
                 'pc-quality': 'high'
             }
-
-            # Create a new task
-            task_id = self.create_task(self.local_image_dir, task_options)
+            transaction_image_dir = os.path.join(self.image_dir, transaction_id)
+            if not os.path.exists(transaction_image_dir):
+                self.logger.log_warning(f"Image directory {transaction_image_dir} does not exist. Exiting.")
+                return None
+            task_id = self.create_task_new_init(task_options)
             if task_id is None:
                 self.logger.log_warning("Calculating the orthophoto failed.")
-                return
-            status_code = 0
-            while status_code != 40 and status_code != 30 and status_code != 50:
-                status_code = self.get_task_status(task_id)
-                if status_code is None:
-                    self.logger.log_warning("Error getting task status.")
-                    return
-                time.sleep(60)
-
-            if status_code == 30 or status_code == 50:
-                self.logger.log_warning(switch_case(status_code))
-                return
-            if status_code == 40:
-                self.logger.log_info(switch_case(status_code))
-                os.makedirs(self.odm_output_dir, exist_ok=True)
-                self.download_results(task_id, self.odm_output_dir)
-                self.remove_task(task_id)
+                return None
+            asyncio.create_task(self.process_task(task_id, transaction_image_dir))
+            return task_id  # Return the task_id
 
         except Exception as e:
             self.logger.log_warning(f"An error occurred: {str(e)}")
